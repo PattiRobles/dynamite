bdb40882d55cbf08fc995e53116f947d74670f58
Patricia Robles <patriciarobles.tech@gmail.com>
2023-01-20 16:33:29 +0000

    removal of unused code / README file added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dynamite!
+
+Simple JavaScript bot to play Dynamite, an enhanced version of Rock, Paper, Scissors, which includes the use of Dynamite and Water.
+
+Very simplified strategy resulting in a bootcamp ruler bot 👑 !!
+
+Test the bot locally using the console. To battle a bot, run the dynamite-cli.js file and give the relative file paths of the two bots of your choice, eg.   `node .\dynamite-cli.js .\myBot.js .\paperBot.js`. 
--- myBot.js
+++ myBot.js
@@ -1,10 +1,9 @@
 class Bot {
     makeMove(gamestate) {
-        let randomIndex1 = Math.floor(5 * Math.random())
-        let randomIndex2 = Math.floor(4 * Math.random())
-        let randomIndex3 = Math.floor(3 * Math.random())
+        let randomIndex1 = Math.floor(4 * Math.random())
+        let randomIndex2 = Math.floor(3 * Math.random())
         let count1 = 0;
-        let count2 = 0;
+
         for (let round of gamestate.rounds) {
             if (round.p1 === 'D') {
                 count1++
@@ -12,14 +11,15 @@ class Bot {
             if (round.p2 === 'D') {
                 count2++
             }
+
         }
 
         if (gamestate.rounds.length >= 1 && gamestate.rounds[gamestate.rounds.length - 1].p1 === gamestate.rounds[gamestate.rounds.length - 1].p2 && count1 < 100) {
-            return ['R', 'P', 'S', 'D'][randomIndex2];
+            return ['R', 'P', 'S', 'D'][randomIndex1];
         }
 
         else {
-            return ['R', 'P', 'S'][randomIndex3];
+            return ['R', 'P', 'S'][randomIndex2];
         }
     }
 }
